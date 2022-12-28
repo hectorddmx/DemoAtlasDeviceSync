a80818fbffdaf898d636f17508fc2737111af85e
Héctor De Diego <lecksfrawen@gmail.com>
2022-12-28 14:31:13 -0600

    fix: bad implementation

--- RealTimeSyncPoCiOS/frontend/swiftui.todo.flex/App/AppConfig.swift
+++ RealTimeSyncPoCiOS/frontend/swiftui.todo.flex/App/AppConfig.swift
@@ -11,15 +11,26 @@ struct AppConfig {
 
 /// Read the Realm.plist file and store the app ID and baseUrl to use elsewhere.
 func loadAppConfig() -> AppConfig {
-    guard let path = Bundle.main.path(forResource: "Realm", ofType: "plist") else {
+    guard
+        let path = Bundle.main.path(
+            forResource: "Realm",
+            ofType: "plist"
+        ),
+        FileManager.default.fileExists(atPath: path)
+    else {
         fatalError("Could not load Realm.plist file!")
     }
     // Any errors here indicate that the Realm.plist file has not been formatted properly.
     // Expected key/values:
     //      "appId": "your Realm app ID"
-    let data = NSData(contentsOfFile: path)! as Data
-    let realmPropertyList = try! PropertyListSerialization.propertyList(from: data, format: nil) as! [String: Any]
-    let appId = realmPropertyList["appId"]! as! String
-    let baseUrl = realmPropertyList["baseUrl"]! as! String
+    guard
+        let data = NSData(contentsOfFile: path) as? Data,
+        let realmPropertyList = try? PropertyListSerialization
+            .propertyList(from: data, format: nil) as? [String: Any],
+        let appId = realmPropertyList["appId"] as? String,
+        let baseUrl = realmPropertyList["baseUrl"] as? String
+    else {
+        fatalError("Could not load valid information from the Realm.plist file")
+    }
     return AppConfig(appId: appId, baseUrl: baseUrl)
 }
